09d2b35913af422e790d03e5369feef2888f5f58
BricenFord <bricencodes@gmail.com>
2024-11-16 12:48:16 -0600

    project5 v0.1.0
    Added locations to fill in project description, LLM documentation, and Debugging and Testing Plan. Imported all necessary (and permitted) libraries.

---
+++ project5/project5.cpp
@@ -0,0 +1,28 @@
+/*
+Project Description:
+
+*/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <unordered_map>
+#include <map>
+#include <iterator>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+
+/*
+LLM Documentation:
+
+*/
+/*
+Debugging and Testing Plan:
+
+*/
